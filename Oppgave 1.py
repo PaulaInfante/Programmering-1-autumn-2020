c314920ca4b1eea1da10a8a585a37063374fcafc
Paula Infante <135032720+PaulaInfante@users.noreply.github.com>
2023-06-12 22:34:20 +0100

    Add files via upload

---
+++ Oppgave 1.py
@@ -0,0 +1,16 @@
+# Task 1a)
+a = 10
+b = 20
+c = 10
+
+if a == c:
+    print("Yes")
+else:
+    print("no")
+
+#Task 1b)
+
+if a == b:
+    print("A dyslexic man walks into a bra.")
+elif a == c:
+    print("(^_^) [0_0] (^.^)")
