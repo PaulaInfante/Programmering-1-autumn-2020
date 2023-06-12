612a003b63e0635a98d7e74270c3b62ab3f65956
Paula Infante <135032720+PaulaInfante@users.noreply.github.com>
2023-06-12 22:36:37 +0100

    Add files via upload

---
+++ oppgave1.py
@@ -0,0 +1,10 @@
+def loop_print(list):
+    """
+    Denne funksjonen tar en liste som et argument og looper igjennom listen og printer ut alle elementene i listen. 
+    """
+    for element in list:
+        print(element)
+
+list1 = ["Element 1", "Element 2", "Element 3"]
+
+loop_print(list1)
